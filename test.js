a26ffe30d2b9275516bb550de883c275d098a185
akshayakola <115446025+akshayakola@users.noreply.github.com>
2024-08-22 11:39:06 +0530

    to-do-list

--- test.js
+++ test.js
@@ -1,100 +1,63 @@
-// typewriter text
-const typewriter_text = ["Welcome To My Blog!!", "By Akhi"];
-
-// responsive navigation
-const navSlide = () => {
-	const burger = document.querySelector ('.burger');
-	const nav = document.querySelector ('.nav-links');
-	const navlinks = document.querySelectorAll ('.nav-links li');
-
-	burger.addEventListener ('click', () => {
-		// toggle nav
-		nav.classList.toggle ('nav-active');
-	
-		// animate links
-		navlinks.forEach ((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-			}
-		});
-		
-		// animate burger
-		burger.classList.toggle ('toggle');
-	});
-}
-
-// sleep function
-function sleep(milliseconds) {
-    let timeStart = new Date().getTime();
-    while (true) {
-      	let elapsedTime = new Date().getTime() - timeStart;
-      	if (elapsedTime > milliseconds) {
-        	break;
-      	}
+// IEFE
+(() => {
+    // state variables
+    let toDoListArray = [];
+    // ui variables
+    const form = document.querySelector(".form");
+    const input = form.querySelector(".form__input");
+    const ul = document.querySelector(".toDoList");
+  
+    // event listeners
+    form.addEventListener('submit', e => {
+      // prevent default behaviour - Page reload
+      e.preventDefault();
+      // give item a unique ID
+      let itemId = String(Date.now());
+      // get/assign input value
+      let toDoItem = input.value;
+      //pass ID and item into functions
+      addItemToDOM(itemId , toDoItem);
+      addItemToArray(itemId, toDoItem);
+      // clear the input box. (this is default behaviour but we got rid of that)
+      input.value = '';
+    });
+  
+    ul.addEventListener('click', e => {
+      let id = e.target.getAttribute('data-id')
+      if (!id) return // user clicked in something else
+      //pass id through to functions
+      removeItemFromDOM(id);
+      removeItemFromArray(id);
+    });
+  
+    // functions
+    function addItemToDOM(itemId, toDoItem) {
+      // create an li
+      const li = document.createElement('li')
+      li.setAttribute("data-id", itemId);
+      // add toDoItem text to li
+      li.innerText = toDoItem
+      // add li to the DOM
+      ul.appendChild(li);
     }
-}
-
-let textIter = 0;
-
-// typewriter effect - text deletion
-function deletingEffect () {
-	let word = typewriter_text[textIter].split ("");
-	var loopDeleting = function () {
-		if (word.length > 0) {
-			word.pop ();
-			document.getElementById ('typewriter-text').innerHTML = word.join ("");
-		} else {
-			if (typewriter_text.length > (textIter + 1)) {
-				textIter++;
-			} else {
-				textIter = 0;
-			}
-
-			typingEffect ();
-			return false;
-		}
-
-		setTimeout (loopDeleting, 150);
-	}
-
-	loopDeleting ();
-}
-
-// typewriter effect - text writing
-function typingEffect () {
-	let word = typewriter_text[textIter].split ("");
-	var loopTyping = function () {
-		if (word.length > 0) {
-			document.getElementById ('typewriter-text').innerHTML += word.shift();
-		} else {
-			sleep (1000);
-			deletingEffect ();
-			return false;
-		}
-
-		setTimeout (loopTyping, 150);
-	}
-
-	loopTyping ();
-}
-
-// scroll indicator
-function UpdateScrollProgress () {
-	var toppos = document.documentElement.scrollTop;
-	var remaining = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-	var percentage = (toppos / remaining) * 100;
-
-	document.getElementsByClassName ('progress-bar')[0].style.width = percentage + "%";
-}
-
-// scroll event listener
-document.addEventListener ("scroll", () => {
-	UpdateScrollProgress ();
-})
-
-// calling functions
-typingEffect ();
-navSlide ();
+  
+    function addItemToArray(itemId, toDoItem) {
+      // add item to array as an object with an ID so we can find and delete it later
+      toDoListArray.push({ itemId, toDoItem});
+      console.log(toDoListArray)
+    }
+  
+    function removeItemFromDOM(id) {
+      // get the list item by data ID
+      var li = document.querySelector('[data-id="' + id + '"]');
+      // remove list item
+      ul.removeChild(li);
+    }
+  
+    function removeItemFromArray(id) {
+      // create a new toDoListArray with all li's that don't match the ID
+      toDoListArray = toDoListArray.filter(item => item.itemId !== id);
+      console.log(toDoListArray);
+    }
+  
+  })();
